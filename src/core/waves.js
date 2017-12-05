a64f5b4352d31d81d0e9debe912c9c8ba787bdcc
Darius Costolas <dariuscostolas@yahoo.com>
2017-12-05 16:21:51 +0200

    Better encapsulate waves handler

--- src/core/waves.js
+++ src/core/waves.js
@@ -6,7 +6,6 @@ const CryptoJS = require('crypto-js');
 const Base58 = require('bs58');
 const axlsign = require('axlsign');
 
-var waves = {};
 
 // private version of getNetworkId byte in order to avoid circular dependency
 // between cryptoService and utilityService
@@ -53,143 +52,144 @@ var prepareKey = function (key) {
 };
 
 
+var waves = {
 
-// blake2b 256 hash function
-waves.blake2b = function (input) {
-    return blake2b(input, null, 32);
-};
-
-// keccak 256 hash algorithm
-waves.keccak = function (messageBytes) {
-    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
-    return keccak_256.array(messageBytes);
-    // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
-};
-
-waves.sha256 = sha256;
-
-waves.hashChain = function (noncedSecretPhraseBytes) {
-    return waves.keccak(waves.blake2b(new Uint8Array(noncedSecretPhraseBytes)));
-};
-
-// Base68 encoding/decoding implementation
-waves.base58 = {
-    encode: function (buffer) {
-        return Base58.encode(buffer);
+    // blake2b 256 hash function
+    blake2b: function (input) {
+        return blake2b(input, null, 32);
     },
-    decode: function (string) {
-        return Base58.decode(string);
-    }
-};
 
-waves.buildAccountSeedHash = function (seedBytes) {
-    var data = appendNonce(seedBytes);
-    var seedHash = waves.hashChain(data);
+    // keccak 256 hash algorithm
+    keccak: function (messageBytes) {
+        // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+        return keccak_256.array(messageBytes);
+        // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+    },
 
-    return sha256(Array.prototype.slice.call(seedHash));
-};
+    sha256: sha256,
 
-waves.buildKeyPair = function (seedBytes) {
-    var accountSeedHash = waves.buildAccountSeedHash(seedBytes);
-    var p = axlsign.generateKeyPair(accountSeedHash);
+    hashChain: function (noncedSecretPhraseBytes) {
+        return waves.keccak(waves.blake2b(new Uint8Array(noncedSecretPhraseBytes)));
+    },
 
-    return {
-        public: waves.base58.encode(p.public),
-        private: waves.base58.encode(p.private)
-    };
-};
+    // Base68 encoding/decoding implementation
+    base58: {
+        encode: function (buffer) {
+            return Base58.encode(buffer);
+        },
+        decode: function (string) {
+            return Base58.decode(string);
+        }
+    },
 
-waves.buildPublicKey = function (seedBytes) {
-    return waves.buildKeyPair(seedBytes).public;
-};
+    buildAccountSeedHash: function (seedBytes) {
+        var data = appendNonce(seedBytes);
+        var seedHash = waves.hashChain(data);
 
-waves.buildPrivateKey = function (seedBytes) {
-    return waves.buildKeyPair(seedBytes).private;
-};
+        return sha256(Array.prototype.slice.call(seedHash));
+    },
 
-waves.buildRawAddress = function (encodedPublicKey) {
-    var publicKey = waves.base58.decode(encodedPublicKey);
-    var publicKeyHash = waves.hashChain(publicKey);
+    buildKeyPair: function (seedBytes) {
+        var accountSeedHash = waves.buildAccountSeedHash(seedBytes);
+        var p = axlsign.generateKeyPair(accountSeedHash);
 
-    var prefix = new Uint8Array(2);
-    prefix[0] = constants.ADDRESS_VERSION;
-    prefix[1] = getNetworkIdByte();
+        return {
+            public: waves.base58.encode(p.public),
+            private: waves.base58.encode(p.private)
+        }
+    },
 
-    var unhashedAddress = appendUint8Arrays(prefix, publicKeyHash.slice(0, 20));
-    var addressHash = waves.hashChain(unhashedAddress).slice(0, 4);
+    buildPublicKey: function (seedBytes) {
+        return waves.buildKeyPair(seedBytes).public;
+    },
 
-    return waves.base58.encode(appendUint8Arrays(unhashedAddress, addressHash));
-};
+    buildPrivateKey: function (seedBytes) {
+        return waves.buildKeyPair(seedBytes).private;
+    },
 
-waves.buildRawAddressFromSeed = function (secretPhrase) {
-    var publicKey = waves.getPublicKey(secretPhrase);
+    buildRawAddress: function (encodedPublicKey) {
+        var publicKey = waves.base58.decode(encodedPublicKey);
+        var publicKeyHash = waves.hashChain(publicKey);
 
-    return waves.buildRawAddress(publicKey);
-};
+        var prefix = new Uint8Array(2);
+        prefix[0] = constants.ADDRESS_VERSION;
+        prefix[1] = getNetworkIdByte();
 
-//Returns publicKey built from string
-waves.getPublicKey = function (secretPhrase) {
-    return waves.buildPublicKey(converters.stringToByteArray(secretPhrase));
-};
+        var unhashedAddress = appendUint8Arrays(prefix, publicKeyHash.slice(0, 20));
+        var addressHash = waves.hashChain(unhashedAddress).slice(0, 4);
 
-//Returns privateKey built from string
-waves.getPrivateKey = function (secretPhrase) {
-    return waves.buildPrivateKey(converters.stringToByteArray(secretPhrase));
-};
+        return waves.base58.encode(appendUint8Arrays(unhashedAddress, addressHash));
+    },
 
-//Returns key pair built from string
-waves.getKeyPair = function (secretPhrase) {
-    return waves.buildKeyPair(converters.stringToByteArray(secretPhrase));
-};
+    buildRawAddressFromSeed: function (secretPhrase) {
+        var publicKey = waves.getPublicKey(secretPhrase);
 
-// function accepts buffer with private key and an array with dataToSign
-// returns buffer with signed data
-// 64 randoms bytes are added to the signature
-// method falls back to deterministic signatures if crypto object is not supported
-waves.nonDeterministicSign = function (privateKey, dataToSign) {
-    var crypto = window.crypto || window.msCrypto;
-    var random;
-    if (crypto) {
-        random = new Uint8Array(64);
-        crypto.getRandomValues(random);
-    }
+        return waves.buildRawAddress(publicKey);
+    },
 
-    var signature = axlsign.sign(privateKey, new Uint8Array(dataToSign), random);
+    //Returns publicKey built from string
+    getPublicKey: function (secretPhrase) {
+        return waves.buildPublicKey(converters.stringToByteArray(secretPhrase));
+    },
 
-    return waves.base58.encode(signature);
-};
+    //Returns privateKey built from string
+    getPrivateKey: function (secretPhrase) {
+        return waves.buildPrivateKey(converters.stringToByteArray(secretPhrase));
+    },
 
-// function accepts buffer with private key and an array with dataToSign
-// returns buffer with signed data
-waves.deterministicSign = function (privateKey, dataToSign) {
-    var signature = axlsign.sign(privateKey, new Uint8Array(dataToSign));
+    //Returns key pair built from string
+    getKeyPair: function (secretPhrase) {
+        return waves.buildKeyPair(converters.stringToByteArray(secretPhrase));
+    },
 
-    return waves.base58.encode(signature);
-};
+    // function accepts buffer with private key and an array with dataToSign
+    // returns buffer with signed data
+    // 64 randoms bytes are added to the signature
+    // method falls back to deterministic signatures if crypto object is not supported
+    nonDeterministicSign: function (privateKey, dataToSign) {
+        var crypto = window.crypto || window.msCrypto;
+        var random;
+        if (crypto) {
+            random = new Uint8Array(64);
+            crypto.getRandomValues(random);
+        }
 
-waves.verify = function (senderPublicKey, dataToSign, signatureBytes) {
-    return axlsign.verify(senderPublicKey, dataToSign, signatureBytes);
-};
+        var signature = axlsign.sign(privateKey, new Uint8Array(dataToSign), random);
 
-waves.encryptWalletSeed = function (seed, key) {
-    var aesKey = prepareKey(key);
+        return waves.base58.encode(signature);
+    },
 
-    return CryptoJS.AES.encrypt(seed, aesKey);
-};
+    // function accepts buffer with private key and an array with dataToSign
+    // returns buffer with signed data
+    deterministicSign: function (privateKey, dataToSign) {
+        var signature = axlsign.sign(privateKey, new Uint8Array(dataToSign));
 
-waves.decryptWalletSeed = function (cipher, key, checksum) {
-    var aesKey = prepareKey(key);
-    var data = CryptoJS.AES.decrypt(cipher, aesKey);
+        return waves.base58.encode(signature);
+    },
 
-    var actualChecksum = waves.seedChecksum(converters.hexStringToByteArray(data.toString()));
-    if (actualChecksum === checksum)
-        return converters.hexStringToString(data.toString());
-    else
-        return false;
-};
+    verify: function (senderPublicKey, dataToSign, signatureBytes) {
+        return axlsign.verify(senderPublicKey, dataToSign, signatureBytes);
+    },
 
-waves.seedChecksum = function (seed) {
-    return converters.byteArrayToHexString(sha256(seed));
-};
+    encryptWalletSeed: function (seed, key) {
+        var aesKey = prepareKey(key);
 
+        return CryptoJS.AES.encrypt(seed, aesKey);
+    },
+
+    decryptWalletSeed: function (cipher, key, checksum) {
+        var aesKey = prepareKey(key);
+        var data = CryptoJS.AES.decrypt(cipher, aesKey);
+
+        var actualChecksum = waves.seedChecksum(converters.hexStringToByteArray(data.toString()));
+        if (actualChecksum === checksum)
+            return converters.hexStringToString(data.toString());
+        else
+            return false;
+    },
+
+    seedChecksum: function (seed) {
+        return converters.byteArrayToHexString(sha256(seed));
+    },
+}
 module.exports = waves;
